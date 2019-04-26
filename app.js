87dc4f26e26763ee7ac2e3fc25654bab4aa83082
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-04-26 21:49:09 +0800

    add dice roll

--- app.js
+++ app.js
@@ -6,3 +6,33 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// var current = document.querySelector("#score-0");
+// current.textContent = dice;
+// document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`;
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = `dice-${dice}.png`;
+
+  // 3. Update the round score IF the rolled number was not a 1
+});
